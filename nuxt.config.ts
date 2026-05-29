26d1865f36429ccc480e16ff5bf0826f5cdf8c58
Ali <alisher.abdimuminov.2005@gmail.com>
2026-05-29 19:02:36 +0500

    bug fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: false },
+    compatibilityDate: "2024-11-01",
+    devtools: { enabled: false },
 
-  css: ["~/assets/css/main.css"],
+    css: ["~/assets/css/main.css"],
 
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-  },
-})
+
+    app: {
+        head: {
+            script: [
+                {
+                    src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1156318250534352",
+                    crossorigin: "anonymous",
+                },
+            ],
+        },
+    },
+});
